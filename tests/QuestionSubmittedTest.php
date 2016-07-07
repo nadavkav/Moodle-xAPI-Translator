fa13724c3f92e9a246ed6385bb0586319556591e
Andrew Downes <mrdownes@hotmail.com>
2016-07-07 15:28:21 +0100

    12 in fact

--- tests/QuestionSubmittedTest.php
+++ tests/QuestionSubmittedTest.php
@@ -224,8 +224,7 @@ class QuestionSubmittedTest extends AttemptStartedTest {
     protected function assertOutputs($input, $output) {
         //output is an associative array
         $this->assertEquals(0, count(array_filter(array_keys($output), 'is_string')));
-        //length of output is 3.
-        $this->assertEquals(11 , count($output));
+        $this->assertEquals(count($input->questions) + 1 , count($output));
     }
 
     protected function assertOutput($input, $output) {
